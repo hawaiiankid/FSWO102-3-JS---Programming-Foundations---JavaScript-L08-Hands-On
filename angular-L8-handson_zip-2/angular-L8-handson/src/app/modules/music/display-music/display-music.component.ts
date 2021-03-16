8f4a90ec2447bf04178164274cf9319f6f9a15c2
Sean <74019460+hawaiiankid@users.noreply.github.com>
2021-03-16 11:29:58 -0700

    Add files via upload

---
+++ angular-L8-handson_zip-2/angular-L8-handson/src/app/modules/music/display-music/display-music.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Music } from '../../../shared/models/music';
+
+
+@Component({
+  selector: 'app-display-music',
+  templateUrl: './display-music.component.html',
+  styleUrls: ['./display-music.component.css']
+})
+export class DisplayMusicComponent implements OnInit {
+
+private musicRoute = 'http://localhost:3000/music';
+public music: Music[];
+
+  constructor(private http: HttpClient) { }
+
+  getMusic(){
+    this.http.get<Music[]>(this.musicRoute).subscribe(music => {
+      this.music = music;
+      console.log('Music', this.music);
+    });
+  }
+
+  ngOnInit() {
+    this.getMusic();
+  }
+
+}
